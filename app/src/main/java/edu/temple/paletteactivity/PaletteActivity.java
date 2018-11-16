8c93d8eef9aedff7c35a22c2070ab822008be7f8
joelj <tug70074@temple.edu>
2018-11-16 12:39:58 -0500

    asdf

--- app/src/main/java/edu/temple/paletteactivity/PaletteActivity.java
+++ app/src/main/java/edu/temple/paletteactivity/PaletteActivity.java
@@ -1,24 +1,9 @@
 package edu.temple.paletteactivity;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
+
 import android.support.v4.app.Fragment;
-import android.app.Activity;
-import android.app.FragmentTransaction;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity implements SpinnerFragment.GetColorInterface {
     CanvasFragment canvasFragment;
